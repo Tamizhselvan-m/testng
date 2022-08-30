158dd01163a5cda189b06a573ea24ef08066615a
tamizhselvan-m <letter2tamizh@gmail.com>
2022-08-30 19:31:28 +0530

    second commit

--- src/test/java/com/testng/TestNG/Test1.java
+++ src/test/java/com/testng/TestNG/Test1.java
@@ -1,5 +1,37 @@
 package com.testng.TestNG;
 
-public class Test1 {
+import org.testng.annotations.Test;
 
+public class Test1 {
+	
+	@Test(priority = -1)
+	private void iPhone() {
+		System.out.println("iphone");
+	}
+	@Test(invocationCount = 3)
+	private void earPhone() {
+		System.out.println("ear phone");
+	}
+	@Test(priority = 1)
+	private void powerBank() {
+		System.out.println("power bank");
+	}
+	@Test(enabled = false)
+	private void dataCard() {
+		System.out.println("data card");
+	}
+	@Test
+	private void laptop() {
+		System.out.println("Laptop");
+	}
+	@Test(priority = 0, expectedExceptions = NullPointerException.class)
+	private void mstring() {
+		String s= null;
+		System.out.println(s.length());
+	}
+	@Test(timeOut = 2000)
+	private void times() throws InterruptedException {
+		Thread.sleep(5000);
+		System.out.println("time");
+	}
 }
